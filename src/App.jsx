4d2da2ad261a32713860bceff301529d7bae2ca7
brhnam ghasemi <behnam.ghasemi082@gmail.com>
2024-02-14 00:13:10 +0330

    check repite password

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function Forms() {
   const [loginPage, setLoginPage] = useState(true);
   const [mobile, setMobile] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordRepite, setPasswordRepite] = useState(true);
   const [validateMobile, setValidateMobile] = useState(true);
   const [validatePassword, setValidatePassword] = useState(true);
   const phoneRegex =
@@ -35,7 +36,13 @@ function Forms() {
       setValidatePassword(false);
     }
   };
-  console.log(password);
+  const handlePasswordRepite = (event) => {
+    if (password === event.target.value) {
+      setPasswordRepite(true);
+    } else {
+      setPasswordRepite(false);
+    }
+  };
   return (
     <div className=" w-3/4 lg:w-2/6  m-auto h-3/4 bg-[#f1f7fe] shadow-lg rounded-3xl relative">
       <div className="w-16 h-16 bg-[#3d4785] absolute -top-6 -right-[20px] rounded-full border-8 border-[#f1f7fe] ">
@@ -126,7 +133,11 @@ function Forms() {
                 type="password"
                 className="focus:outline-none"
                 placeholder="*******"
+                onInput={handlePasswordRepite}
               />
+              {passwordRepite == false && (
+                <p className="text-xs text-red-500">رمز عبور یکسان نیست</p>
+              )}
             </div>
           </div>
           <button className="w-2/3 h-16  bg-[#3d4785] text-white rounded-full">
